2c81d6707656060d8a73b2ff63413a71cd2e818b
CurtisChou-51 <71554373+CurtisChou-51@users.noreply.github.com>
2025-01-01 16:02:13 +0800

    first commit

---
+++ WindowsServiceExample/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WindowsServiceExample
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new { alive = true });
+        }
+    }
+}
---
+++ WindowsServiceExample/Services/Example2ShellService.cs
@@ -0,0 +1,41 @@
+﻿
+namespace WindowsServiceExample.Services
+{
+    public class Example1ShellService : IBgShellService
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly SemaphoreSlim _semaphoreSlim;
+
+        public Example1ShellService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _semaphoreSlim = new(1);
+        }
+
+        /// <summary> Execute (如正在執行時再次呼叫則不執行並return false) </summary>
+        public bool Execute()
+        {
+            if (!_semaphoreSlim.Wait(0))
+                return false;
+
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await ExecuteImpl();
+                }
+                finally
+                {
+                    _semaphoreSlim.Release();
+                }
+            });
+            return true;
+        }
+
+        /// <summary> 執行 </summary>
+        private Task ExecuteImpl()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
