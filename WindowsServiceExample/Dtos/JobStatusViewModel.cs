09e6288b7bf41e034064d25128d6aacee541695d
CurtisChou-51 <71554373+CurtisChou-51@users.noreply.github.com>
2025-03-19 14:36:22 +0800

    feat: add query api

---
+++ WindowsServiceExample/Dtos/JobStatusViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WindowsServiceExample.Dtos
+{
+    public class JobStatusViewModel
+    {
+        public string JobIdentity { get; set; }
+        public string JobName { get; set; }
+        public string CronExpression { get; set; }
+        public bool IsRunning { get; set; }
+    }
+
+}
